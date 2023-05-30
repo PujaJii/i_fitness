89539dee7c2d69467924dbe0e497226c49710b37
Pujajii <puja.technext@gmail.com>
2023-05-30 18:19:24 +0530

    30-05-2023

--- lib/apis/video_api.dart
+++ lib/apis/video_api.dart
@@ -1,4 +1,3 @@
-import 'package:global_configuration/global_configuration.dart';
 import 'package:http/http.dart' as http;
 import '../models/video_model.dart';
 
@@ -11,7 +10,9 @@ class VideoApi{
     print('cat_id in api: $cat_id');
    // var baseUrl = GlobalConfiguration().get('base_url');
 
-    var response = await client.post(Uri.parse('https://iwebnext.us/fitness/public/api/video'), body: {'cat_id': cat_id});
+    var response = await client.post(
+        Uri.parse('https://iwebnext.us/fitness/public/api/video'),
+        body: {'cat_id': cat_id});
 
     if (response.statusCode == 200) {
       var jsonString = response.body;
